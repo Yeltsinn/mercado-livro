b7a864003c26e481e9cef2ffa943fd6e05a2efdd
Yeltsin Lobato <yeltsin.lobato@gmail.com>
2023-07-05 11:11:14 -0300

    Criando modelo para Customer e substituindo retorno de método get pela modelo criada

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,13 +1,16 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("customers")
 class CustomerController {
 
     @GetMapping
-    fun helloWorld(): String {
-        return "Hello World"
+    fun helloWorld(): CustomerModel {
+        return CustomerModel("1", "Gustavo", "gustavo@gmail.com")
     }
 }
