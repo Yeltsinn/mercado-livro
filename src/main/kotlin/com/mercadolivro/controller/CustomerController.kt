4d4279ab083564d93fa7997664f5b97fe4813558
Yeltsin Lobato <yeltsin.lobato@gmail.com>
2023-07-06 08:52:38 -0300

    implementando recebimento de parametros via path parameter and implementando metódo PUT

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -4,7 +4,9 @@ import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -14,14 +16,34 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("customers")
 class CustomerController {
 
+    val customers = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun getCustomer(): CustomerModel {
-        return CustomerModel("1", "Gustavo", "gustavo@gmail.com")
+    fun getAll(): MutableList<CustomerModel> {
+        return customers
+    }
+
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel {
+        return customers.first { it.id == id }
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: String, @RequestBody putCustomerRequest: PostCustomerRequest) {
+        val customer = customers.first { it.id == id }.let {
+            it.name = putCustomerRequest.name
+            it.email = putCustomerRequest.email
+        }
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest) {
-        println(customer)
+
+        val indexOfNewCostumer = customers.count() + 1
+
+        val newCustomer = CustomerModel(indexOfNewCostumer.toString(), customer.name, customer.email)
+        customers.add(newCustomer)
     }
 }
